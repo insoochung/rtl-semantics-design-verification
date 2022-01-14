e4e1f8fe530c75ec218b8382ff8f53473c8ca083
Insoo Chung <insoo.chung@tamu.edu>
2022-01-13 21:11:16 -0600

    Fix bugs in code parser

--- parser/code_parser.py
+++ parser/code_parser.py
@@ -7,6 +7,9 @@ BASE_DIR = os.path.abspath(
 IBEX_DIR = os.path.join(BASE_DIR, "ibex")
 RTL_DIR = os.path.join(IBEX_DIR, "rtl")
 
+def has_token(line, token):
+  return token in line.split("//")[0].split() # comment is not taken into account
+
 def get_modules_from_file(filepath):
   """ Return a list of module strings from a file """
   modules = {}
@@ -23,7 +26,7 @@ def get_modules_from_file(filepath):
       start_linenum = i + 1
     
     if do_append:
-      module_str += " ".join(line.split()) + "\n"
+      module_str += line
     
     if line.startswith("endmodule"):
       do_append = False
@@ -42,24 +45,40 @@ def get_always_blocks_from_modules(module_str, line_num):
   always_linenum = 0
   cnt_begin = 0
   for i, line in enumerate(module_str.split("\n")):
-    if line.startswith("always"):
+    if line.strip().startswith("always"):
       do_append = True
       always_linenum = line_num + i
     
     if do_append:
       always_str += line + "\n"
       # We are assuming one statement for a line in an always block.
-      assert line.count(";") <= 1 or line.startswith("for"), (
+      assert line.count(";") <= 1 or line.strip().startswith("for"), (
         "Line '{}' has more than one semicolon".format(line))
-      if "begin" in line:
+      if has_token(line, "begin"):
         cnt_begin += 1
-      if "end" in line:
+      if has_token(line, "end"):
         cnt_begin -= 1
 
       if cnt_begin == 0: # end of always block
         do_append = False
         if always_str:
+          always_str = always_str.rstrip()
+          # Make sure that we are saving a complete always block
+          first_line = always_str.split("\n")[0] # this should begin with "always"
+          last_line = always_str.split("\n")[-1] # this should be equal to "end"
+          assert (len(first_line) - len(first_line.lstrip(' '))
+                  == len(last_line) - len(last_line.lstrip(' '))), (
+                    "Indent should be the same for start and end of the always block.",
+                    always_str, always_linenum
+                  )
+          last_line = last_line.split("//")[0].strip() # remove comments
+          last_line = last_line.split(":")[0].strip() # remove ": sync_write"
+          assert first_line.strip().startswith("always") and last_line.strip() == "end", (
+                    "Always block should start with 'always' and end with 'end'"
+                  )
           always_blocks.append((always_linenum, always_str))
+          
+
         always_str = ""
 
   return always_blocks
@@ -85,6 +104,8 @@ def parse(rtl_dir=RTL_DIR, verbose=True):
   for filename, modules in files_to_modules.items():
     res[filename] = {}
     for module_name, (line_num, module_str) in modules.items():
+      log_fn("Always extraction: filename={}, modulename={}".format(
+        filename, module_name))
       always_blocks = get_always_blocks_from_modules(module_str, line_num)
       log_fn("Always extraction: filename={}, modulename={}, # always={}".format(
         filename, module_name, len(always_blocks)))
