e7f7596a189deb6aab8124e0b70a7a560297cd3e
Insoo Chung <insoo.chung@tamu.edu>
2022-01-16 17:40:06 -0600

    Fix path errors

--- parser/config.py
+++ parser/config.py
@@ -1,8 +1,11 @@
 import os
 
 BASE_DIR = os.path.abspath(
-  os.path.dirname(
-    os.path.dirname(__file__)))
+    os.path.dirname(
+        os.path.dirname(__file__)))
+
+PARSER_DIR = os.path.join(BASE_DIR, "parser")
 IBEX_DIR = os.path.join(BASE_DIR, "ibex")
+
 RTL_DIR = os.path.join(IBEX_DIR, "rtl")
-LARK_RULES = os.path.join(BASE_DIR, "lark_rules.lark")
+LARK_RULES = os.path.join(PARSER_DIR, "lark_rules.lark")
