6b6555356382b46ac9b9f56aa66ca91e89b0d6ac
Aaron Chan <aarongchan@login2.cluster>
2022-04-15 12:44:46 -0500

    Address comments

--- nn/tp_adjustment.py
+++ nn/tp_adjustment.py
@@ -30,14 +30,15 @@ def output_yaml(vocab, tp, cp_idx):
   test: {adjusted_test_name}
   """
   gen_opts_str = (f"test: {adjusted_test_name} \n") + gen_opts_str
-  with open(f"{adjusted_test_name}.yaml", "a") as f:
+  with open(f"{adjusted_test_name}.yaml", "w+") as f:
     f.write(gen_opts_str)
 
-def adjust_tp(graph_dir, ckpt_dir, test_parameters, tp_cov_dir, cp_idx):
+def adjust_tp(graph_dir, ckpt_dir, tp_cov_dir, cp_idx):
   vocab_files = glob(os.path.join(tp_cov_dir, "vocab.*.yaml"))
   vocab = TestParameterVocab(vocab_filepath=vocab_files[1])
-  tp_path = test_parameters
-  tp = vocab.vectorize_test(tp_path)
+  tp_size = len(vocab.tokens)
+
+  tp = np.random.rand(tp_size)
   design_graph_filepath = os.path.join(graph_dir, "design_graph.pkl")
   graph_handler = GraphHandler(design_graph_filepath, output_dir=graph_dir)
   graphs = graph_handler.get_dataset()
@@ -50,32 +51,45 @@ def adjust_tp(graph_dir, ckpt_dir, test_parameters, tp_cov_dir, cp_idx):
   model = get_d2v_model(**model_config)
   model.load_weights(os.path.join(ckpt_dir, "model.ckpt"))
 
-  coverage = 0
-  prev_coverage = 1
-  while abs(prev_coverage - coverage) > .01:
-    prev_coverage = coverage
-    x = update_tp(tp, 1, graphs, bvocab)
-    with tf.GradientTape() as g:
-      g.watch(x)
-      y = model.call(x)
-    coverage = y[[0]]
-    dy_dx = g.gradient(y, x)["test_parameters"]
-    tp += dy_dx * 10
-
-  tp = tp.numpy()[0]
+  is_hit = 0
+  lr = 1
+  step_threshold = 50
+  for step in range(step_threshold):
+    if step == step_threshold - 1:
+      while(is_hit < .9):
+        x = update_tp(tp, cp_idx, graphs, bvocab, tp_size)
+        with tf.GradientTape() as g:
+          g.watch(x)
+          y = model.call(x)
+          loss = y[[0]] - 1
+        is_hit = y[[0]]
+        dy_dx = g.gradient(loss, x)["test_parameters"]
+        tp += dy_dx * lr
+    else:
+      x = update_tp(tp, cp_idx, graphs, bvocab, tp_size)
+      with tf.GradientTape() as g:
+        g.watch(x)
+        y = model.call(x)
+        loss = y[[0]] - 1
+      is_hit = y[[0]]
+      dy_dx = g.gradient(loss, x)["test_parameters"]
+      tp += dy_dx * lr
+  tp = [1 if i > 1 else 0 if i < 0 else i for i in tp.numpy()[0]]
   output_yaml(vocab, tp, cp_idx)
-def update_tp(tp, cp_idx, graphs, bvocab):
+def update_tp(tp, cp_idx, graphs, bvocab, tp_size):
     midx = bvocab.get_module_index(cp_idx)
-    mask = bvocab.get_mask(cp_idx, midx, mask_length=graphs[0].n_nodes)
+    mask_length = graphs[0].n_nodes
+    mask = bvocab.get_mask(cp_idx, midx, mask_length=mask_length)
     mask = np.ma.make_mask(mask)
     is_hit = np.array([1])
     gidx = np.array([midx])
     cp = np.array([cp_idx])
-
+    if isinstance(tp, tf.Tensor):
+      tp = [1 if i > 1 else 0 if i < 0 else i for i in tp.numpy()[0]]
     ret = {
-      "test_parameters": np.array([tp]).reshape(1, 58),
+      "test_parameters": np.array([tp]).reshape(1, tp_size),
       "is_hits": is_hit.reshape(1, 1),
-      "coverpoint_mask": mask.reshape(1, 740),
+      "coverpoint_mask": mask.reshape(1, mask_length),
       "graph": gidx.reshape(1,),
       "coverpoint": cp.reshape(1,)
     }
@@ -86,20 +100,16 @@ def update_tp(tp, cp_idx, graphs, bvocab):
       tf_ret[k] = tf.constant(v, dtype=v.dtype)
     
     return tf_ret
-
-  
 if __name__ == "__main__":
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
     parser = argparse.ArgumentParser()
     parser.add_argument("-gd", "--graph_dir", type=str, required=True,
                       help="Directory of the graph dataset.")
     parser.add_argument("-cd", "--ckpt_dir", type=str, required=True,
-                      help="Directory to save the checkpoints.")
-    parser.add_argument("-tp", "--test_parameters", type=str, required=True,
-                      help="Test paramters .yaml path")         
+                      help="Directory to save the checkpoints.")       
     parser.add_argument("-td", "--tp_cov_dir", type=str,
                       help="Directory of the test parameter coverage dataset.")
-    parser.add_argument("-cp", "--cp_idx", type=str,
+    parser.add_argument("-cp", "--cp_idx", type=int,
                       help="Coverpoint index.")
     args = parser.parse_args()
     adjust_tp(**vars(args))
